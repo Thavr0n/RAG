a5a2ae3d46dae5814e3026ea48d4cf9d8cfae615
Thavr0n <ssaman.mardi@gmail.com>
2024-06-29 06:33:34 +0200

    Templates

--- FragMich.py
+++ FragMich.py
@@ -31,8 +31,9 @@ llm = ChatOllama(model=local_model)
 # zu dem Queryvektor aus der User*innenanfrage rauszusuchen und zu formulieren
 QUERY_PROMPT = PromptTemplate(
     input_variables=["question"],
-    template="""You are an AI language model assistant. You answer in german only. 
-    All the documents are in german. Your task is to generate five different versions 
+    template="""You are an AI language model assistant. 
+    You answer in german only. All the documents are in german. 
+    Your task is to generate five different versions 
     of the given user question to retrieve relevant documents from a vector database. 
     By generating multiple perspectives on the user question, your goal is to help the 
     user overcome some of the limitations of the distance-based similarity search. 
