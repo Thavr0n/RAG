473231c3ad36822cf51e3310bb2e743cf61e2e37
Thavr0n <ssaman.mardi@gmail.com>
2024-07-01 04:03:57 +0200

    update auf dt. sprachmodell  (s. readme)

--- FragMich.py
+++ FragMich.py
@@ -19,7 +19,7 @@ print("Lade Vektordatenbank mit grünem Wissen...")
 ## legt das LLM Modell fest mit dem wir arbeiten
 print("Lade LLM Model...")
 try:
-    local_model ="llama3:latest"
+    local_model ="GermanLlama3:latest"  ##ursprgl. stand hier "llama3:latest" - aktuell tests mit anderen modellen s. readme.md
 except Exception as e:
     print(f"Error: LLM model loading failed - {e}")
 print("LLM Model erfolgreich geladen!")
@@ -31,14 +31,21 @@ llm = ChatOllama(model=local_model)
 # zu dem Queryvektor aus der User*innenanfrage rauszusuchen und zu formulieren
 QUERY_PROMPT = PromptTemplate(
     input_variables=["question"],
-    template="""You are an AI language model assistant. 
-    You answer in german only. All the documents are in german. 
-    Your task is to generate five different versions 
-    of the given user question to retrieve relevant documents from a vector database. 
-    By generating multiple perspectives on the user question, your goal is to help the 
-    user overcome some of the limitations of the distance-based similarity search. 
-    Provide these alternative questions separated by newlines.
-    Original question: {question}""",
+    #falls kein dt. LLM installiert ist (s. readme) funktioniert diese Anweisung stattdessen: 
+    # """You are an AI language model assistant. 
+    # You answer in german only. All the documents are in german. 
+    # Your task is to generate five different versions 
+    # of the given user question to retrieve relevant documents from a vector database. 
+    # By generating multiple perspectives on the user question, your goal is to help the 
+    # user overcome some of the limitations of the distance-based similarity search. 
+    # Provide these alternative questions separated by newlines.
+    # Original question: {question}"""
+    template="""Du bist ein KI-Sprachmodell-Assistent. Deine Aufgabe ist es, fünf verschiedene Versionen 
+    der gegebenen Benutzerfrage zu generieren, um relevante Dokumente aus einer Vektordatenbank zu finden. 
+    Indem Du mehrere Perspektiven auf die Frage des Benutzers erzeugst, hilfst Du dem Benutzer dabei, 
+    einige der Einschränkungen der entfernungsbasierten Ähnlichkeitssuche zu überwinden. 
+    Gib die alternativen Fragen durch Zeilenumbrüche getrennt ein.
+    Ursprüngliche Frage: {question}""",
 )
 
 ## der retriever erlaubt einen String Input, was unser query samt des templates ist 
@@ -57,8 +64,11 @@ print("Retriever erfolgreich erstellt!")
 
 ## Dieser Befehl sagt unserem System, dass es die Frage bzw. das query NUR basierend auf den fünf besten Ergebnissen 
 # unseres retrievers beantworten soll. 
-template = """ Explain your answer in german based ONLY on the following context:{context}
-    Question: {question}"""
+# falls kein dt. LLM installiert ist (s. readme) funktioniert diese Anweisung stattdessen: 
+# """ Explain your answer in german based ONLY on the following context:{context}
+#    Question: {question}"""
+template = """ Begründe Deine Antwort NUR anhand des folgenden Zusammenhangs:{context}
+    Frage: {question}"""
 
 
 prompt = ChatPromptTemplate.from_template(template)
@@ -69,10 +79,10 @@ prompt = ChatPromptTemplate.from_template(template)
 # question ist die Frage, die User*innen eingeben | RunnablePassthrough erlaubt es die Eingabeart erst später festzulegen (https://api.python.langchain.com/en/latest/runnables/langchain_core.runnables.passthrough.RunnablePassthrough.html)
 ## ebf. dazu: https://www.artefact.com/blog/unleashing-the-power-of-langchain-expression-language-lcel-from-proof-of-concept-to-production/
 chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
+    {"context": retriever, "question": RunnablePassthrough()} # der erste teil der chain wandelt den userinput wie in QUERY_PROMPT beschrieben um und zieht den Kontext aus unserer Datenbank
+    | prompt # der zweite Teil der chain sagt dem System, dass es die Frage nur wie unter template beschrieben beantworten soll
+    | llm # der dritte Teil der Chain ruft unser llm auf und übergibt ihm alle Informationen
+    | StrOutputParser() # Ausgabe des Ergebnisses
 )
 
 print("Wozu soll ich Dir etwas sagen?")
